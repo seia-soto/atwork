4f2af084860aedb9b2c60dd2ad312d2a79515c28
Seia-Soto <seia@outlook.kr>
2021-09-05 00:26:58 +0900

    feat(component): make navigation link and remove styles

--- src/pages/components/Navigation.tsx
+++ src/pages/components/Navigation.tsx
@@ -12,10 +12,18 @@ export const Right = styled('div', {
   marginLeft: 'auto',
   gap: '14px'
 })
-export const Item = styled('div', {
+export const Item = styled('a', {
   display: 'inline-flex',
   height: '100%',
+  color: 'inherit',
+  textDecoration: 'none',
 
   justifyContent: 'center',
-  alignItems: 'center'
+  alignItems: 'center',
+
+  transition: 'color .65s ease-in-out',
+
+  '&:hover': {
+    color: '#004D8C'
+  }
 })
--- src/pages/layouts/Navigation.tsx
+++ src/pages/layouts/Navigation.tsx
@@ -5,7 +5,7 @@ import * as Navigation from '../components/Navigation'
 const NavigationLayout: NextPage = () => {
   return (
     <Navigation.Wrapper size='md'>
-      <Navigation.Item css={{ fontWeight: 600, fontSize: '17.5px' }}>
+      <Navigation.Item css={{ fontWeight: 600, fontSize: '17.5px' }} href='/'>
         <Avatar src='/images/avatar.png' css={{ marginRight: '12px', height: '40px' }} />
         HoJeong Go
       </Navigation.Item>
