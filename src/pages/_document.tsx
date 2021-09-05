6728404b61d2b72f23e6bd3b83ff5c2b40b1f04b
Seia-Soto <seia@outlook.kr>
2021-09-06 03:47:56 +0900

    chore: remove font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,15 +4,6 @@ const MyApp = () => {
   return (
     <Html>
       <Head>
-        {
-          /**
-           * (font) Pretendard
-           */
-        }
-        <link
-          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
-          rel="stylesheet"
-        />
       </Head>
       <body>
         <Main />
