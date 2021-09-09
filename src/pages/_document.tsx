0776812631e32d2f40f58c8c54608d31c06479a2
Seia-Soto <seia@outlook.kr>
2021-09-09 20:57:59 +0900

    chore: add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,8 @@ const MyDocument = () => {
       <Head>
         <style id='stitches' dangerouslySetInnerHTML={{ __html: getCssText() }} />
         <link rel='preload' href='/images/avatar.webp' as='image' />
+
+        <script async defer data-domain='seia.io' src='https://analytics.seia.io/a' />
       </Head>
       <body>
         <Main />
