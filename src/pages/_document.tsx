12789efd9a465993aab03c7a8adc34cf78cc562b
seia-soto <seia@outlook.kr>
2021-10-11 03:30:23 +0900

    chore: remove tracker

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,8 +8,6 @@ const MyDocument = () => {
         <style id='stitches' dangerouslySetInnerHTML={{ __html: getCssText() }} />
         <link rel='preload' href='/images/avatar.webp' as='image' />
 
-        <script async defer data-domain='seia.io' src='https://trkrpt-submit.cidx.rpta1-c6h16.seia.io/a' />
-        <script async defer data-domain='seia.io' src='https://trkrpt-submit.cidx.rpta12-c3h5.seia.io/a' />
         <script
           dangerouslySetInnerHTML={{
             __html: 'w=window;if(w.navigator.userAgent.indexOf("Trident")>=0){w.location="/unsupported.html"}'
